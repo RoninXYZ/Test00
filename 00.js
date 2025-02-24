c1745c7717fe93b8e45cc7f86be8614580d6e52a
RoninXYZ <xyzronin97@gmail.com>
2025-02-24 17:37:35 +0700

    Initial commit

--- 00.js
+++ 00.js
@@ -1,11 +1,57 @@
+// server.js
+require("dotenv").config();
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+// Koneksi ke MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
+
+// Schema User
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+const User = mongoose.model("User", UserSchema);
+
+// Routes
 app.get("/", (req, res) => {
-  res.send("Hello from Railway!");
+  res.send("API is running...");
 });
 
-const PORT = process.env.PORT || 3000;
+// Get All Users
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+// Create User
+app.post("/users", async (req, res) => {
+  const { name, email } = req.body;
+  const newUser = new User({ name, email });
+  await newUser.save();
+  res.json(newUser);
+});
+
+// Delete User
+app.delete("/users/:id", async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  res.json({ message: "User deleted" });
+});
+
+// Jalankan Server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
